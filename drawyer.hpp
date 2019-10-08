c4bb16f886ae5e2f88e2fc1a231b0c47de62f8a4
InversionSpaces <InversionSpaces@vivaldi.net>
2019-10-08 20:14:42 +0300

    Can draw a triangle now

--- drawyer.hpp
+++ drawyer.hpp
@@ -7,6 +7,11 @@ struct line_t
     realcoords_t dots[2];
 };
 
+struct triangle_t
+{
+    realcoords_t dots[3];
+};
+
 class Drawyer
 {
 private:
@@ -65,6 +70,67 @@ public:
             //std::cout << i << " " << j << std::endl;
             (*xw)[xORy ? pixelcoords_t({i, j}) : pixelcoords_t({j, i})] = color;
         }
+    }
+    
+    void draw(const triangle_t& triangle, const bgracolor_t& color) 
+    {
+        screencoords_t r0 = cc->real2screen(triangle.dots[0]);
+        screencoords_t r1 = cc->real2screen(triangle.dots[1]);
+        screencoords_t r2 = cc->real2screen(triangle.dots[2]);
+        
+        //std::cout << r0.x << " " << r0.y << std::endl;
+        //std::cout << r1.x << " " << r1.y << std::endl;
+        //std::cout << r2.x << " " << r2.y << std::endl;
+        
+        screencoords_t b = r1 - r0;
+        screencoords_t c = r2 - r0;
+        
+        //std::cout << "b " << b.x << " " << b.y << std::endl;
+        //std::cout << "c " << c.x << " " << c.y << std::endl;
+        
+        float det = sarea(b, c);
+        
+        //std::cout << "det " << det << std::endl;
+        
+        pixelcoords_t r0p = cc->screen2pixel(r0);
+        pixelcoords_t r1p = cc->screen2pixel(r1);
+        pixelcoords_t r2p = cc->screen2pixel(r2);
+        
+        uint16_t xmin = std::min(r0p.x, std::min(r1p.x, r2p.x));
+        uint16_t xmax = std::max(r0p.x, std::max(r1p.x, r2p.x));
+        
+        uint16_t ymin = std::min(r0p.y, std::min(r1p.y, r2p.y));
+        uint16_t ymax = std::max(r0p.y, std::max(r1p.y, r2p.y));
+        
+        //std::cout << xmin << "->" << xmax << std::endl;
+        //std::cout << ymin << "->" << ymax << std::endl;
+        
+        //uint16_t count = 0;
+        for (uint16_t x = xmin; x <= xmax; ++x)
+            for (uint16_t y = ymin; y <= ymax; ++y) {
+                screencoords_t r = cc->pixel2screen({x, y});
+                screencoords_t a = r - r0;
+                
+                float det1 = sarea(a, c);
+                float det2 = sarea(b, a);
+                float det3 = det - det1 - det2;
+                
+                float beta 	= det1 / det;
+                float gamma = det2 / det;
+                float alpha = det3 / det;
+                
+                //std::cout << x << " " << y << std::endl;
+                //std::cout << alpha << " " << beta << " " << gamma << std::endl;
+                
+                //std::cout << "Bad " << x << " " << y << std::endl;
+                if ((alpha > 0) && (beta > 0) && (gamma > 0)) //{ 
+                    //std::cout << "Good " << x << " " << y << std::endl;
+                    (*xw)[{x, y}] = color;
+                    //count++;
+                //}
+            }
+        
+        //std::cout << (xmax - xmin) * (ymax - ymin) << " " << count << std::endl;
     }
     
     void flush()
